f56321120045ed9618a4be02a06ca39ac7172679
kwangyoulsagong <sgky0511@naver.com>
2024-11-21 16:14:43 +0900

    여행 추가 여행 조회컨트롤러  기능

---
+++ controller/trip.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from "express";
+import TripService from "../services/trip";
+import connection from "../db";
+interface DecodedToken {
+  user_id: number;
+  iat: number;
+  exp: number;
+}
+interface AuthRequest extends Request {
+  user?: DecodedToken;
+}
+class TripController {
+  private tripService: TripService;
+  constructor() {
+    this.tripService = new TripService(connection);
+  }
+
+  public createTrip = async (req: AuthRequest, res: Response) => {
+    try {
+      if (!req.user?.user_id) {
+        return res.status(401).json({ error: "인증이 필요합니다." });
+      }
+      const { groupId, startDate, endDate, destination, locations } = req.body;
+
+      const trip = await this.tripService.createTrip(
+        groupId,
+        startDate,
+        endDate,
+        destination,
+        locations
+      );
+
+      res.status(201).json(trip);
+    } catch (error) {
+      console.error("여행 생성 에러:", error);
+      res.status(500).json({ error: "여행 생성에 실패했습니다." });
+    }
+  };
+
+  public getTripDetails = async (req: AuthRequest, res: Response) => {
+    try {
+      const { tripId } = req.params;
+      if (!req.user?.user_id) {
+        return res.status(401).json({ error: "인증이 필요합니다." });
+      }
+      const tripDetails = await this.tripService.getTripDetails(
+        parseInt(tripId)
+      );
+      res.json(tripDetails);
+    } catch (error) {
+      console.error("여행 조회 에러:", error);
+      res.status(500).json({ error: "여행 조회에 실패 했습니다." });
+    }
+  };
+}
+export default TripController;
